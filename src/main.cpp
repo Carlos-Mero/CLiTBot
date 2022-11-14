8900fb384049b90ea64dfea857a7eee39221fe35
2450213803@qq.com <huangyanxing@huangyanxingdeMacBook-Pro.local>
2022-11-14 20:46:07 +0800

    This is another commit for test, I created the main file here.

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+// This is the main entrance of CLiTBot, a project of the course "The Basis of Program Design".
+// There're Three members here in our group, They are Yu Zhan, Huang Yanxing and Wang Yuhan. Thanks for all!
+
+int main(){
+    
+    return 0;
+}
