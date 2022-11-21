8ae65cda3b6cac7da4263ab358ed7b3cdbe7e886
Yellow-GGG <yellow_ggg@icloud.com>
2022-11-21 18:12:03 +0800

    updated frameworks of the game

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 // This is the main entrance of CLiTBot, a project of the course "The Basis of Program Design".
 // There're Three members here in our group, They are Yu Zhan, Huang Yanxing and Wang Yuhan. Thanks for all!
 
-#include "main_bs.cpp"
-#include "main_ex.cpp"
+#include "modes.h"
 
 int main(){
     // This is the main entrance of the game CLiTBot, we can choose between both basic version and extended version of the game here through a graphic user interface.
---
+++ src/modes.h
@@ -0,0 +1,7 @@
+#ifndef _GAME_MODES_
+#define _GAME_MODES_
+
+int main_bs();
+int main_ex();
+
+#endif
